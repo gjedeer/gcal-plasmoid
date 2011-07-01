581b32f81bab7449c2b0e8639a21ca62dd327840
GDR\! <gdr@go2.pl>
2011-07-01 15:28:11 +0200

    Version bump

--- makePlasmoid.sh
+++ makePlasmoid.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 zip  -x ".git/*" -x ".project" -x ".settings/*" -x ".pydevproject"  -x "*/*.pyc" -x ".gitignore" -x "makePlasmoidPkg.sh"  -r ../plasma_gcal.plasmoid .
-scp ../plasma_gcal.plasmoid gdr@pornel.net:public_html/gdrwpl/heavy/
+scp ../plasma_gcal.plasmoid gdr@gdr.name:/srv/gdr.geekhood.net/gdrwpl/heavy/
